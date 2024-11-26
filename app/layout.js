b58029be5742a7be79e373d0c8466876571667f6
Ninism <nin.alamo@outlook.com>
2024-11-26 12:37:14 +0800

    head

--- app/layout.js
+++ app/layout.js
@@ -20,9 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <header>
+      <head>
         <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet"/>
-      </header>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
